23a1f44d88407ac7a67768342c22450946bd1005
김근탁 <newstarTak1@gmail.com>
2022-05-25 21:15:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GumPaper
+
+Developed with Unreal Engine 4
---
+++ Source/GumPaper/GumPaperGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GumPaperGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGumPaperGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGumPaperGameMode();
+};
+
+
+
